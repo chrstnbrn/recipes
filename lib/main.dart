debf25c26de1484863836bf596b8a41e22617bfd
Christina Braun <11138584+chrstnbrn@users.noreply.github.com>
2020-08-22 15:20:57 +0200

    Initialize Firebase app

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_database/firebase_database.dart';
 import 'package:recipes/store/recipe_repository.dart';
@@ -10,13 +11,27 @@ void main() => runApp(RecipesApp());
 class RecipesApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Recipes',
-      theme: appTheme(),
-      home: Recipes(
-          title: 'Recipes',
-          repository:
-              new RecipeRepository(FirebaseDatabase.instance.reference())),
-    );
+    return FutureBuilder(
+        future: Firebase.initializeApp(),
+        builder: (context, snapshot) {
+          if (snapshot.hasError) {
+            return CircularProgressIndicator(
+              backgroundColor: Theme.of(context).errorColor,
+            );
+          }
+
+          // Once complete, show your application
+          if (snapshot.connectionState == ConnectionState.done) {
+            return MaterialApp(
+                title: 'Recipes',
+                theme: appTheme(),
+                home: Recipes(
+                    title: 'Recipes',
+                    repository: new RecipeRepository(
+                        FirebaseDatabase.instance.reference())));
+          }
+
+          return CircularProgressIndicator();
+        });
   }
 }
