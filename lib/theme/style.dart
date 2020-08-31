b7425789d6657a00afbb891d5bbdd5592da7d0ed
Christina Braun <11138584+chrstnbrn@users.noreply.github.com>
2020-08-31 14:14:53 +0200

    Increase font size

--- lib/theme/style.dart
+++ lib/theme/style.dart
@@ -3,11 +3,19 @@ import 'package:flutter/material.dart';
 ThemeData appTheme() {
   return ThemeData.from(
     colorScheme: ColorScheme.light(
-        primary: Colors.green,
-        secondary: Colors.lightGreen.shade500,
-        onPrimary: Colors.white,
-        onSecondary: Colors.white),
+      primary: Colors.green,
+      secondary: Colors.lightGreen.shade500,
+      onPrimary: Colors.white,
+      onSecondary: Colors.white,
+    ),
+    textTheme: const TextTheme(
+      bodyText1: TextStyle(fontSize: 18.0),
+      bodyText2: TextStyle(fontSize: 16.0),
+    ),
   ).copyWith(
-      buttonTheme: const ButtonThemeData(
-          buttonColor: Colors.green, textTheme: ButtonTextTheme.primary));
+    buttonTheme: const ButtonThemeData(
+      buttonColor: Colors.green,
+      textTheme: ButtonTextTheme.primary,
+    ),
+  );
 }
