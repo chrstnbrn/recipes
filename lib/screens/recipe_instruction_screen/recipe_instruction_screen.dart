eb6fc4e06d30ce1770606585532b4de2c66dd5af
Christina Braun <11138584+chrstnbrn@users.noreply.github.com>
2021-02-06 17:52:24 +0100

    Add automatic scroll to next recipe step

--- lib/screens/recipe_instruction_screen/recipe_instruction_screen.dart
+++ lib/screens/recipe_instruction_screen/recipe_instruction_screen.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:scrollable_positioned_list/scrollable_positioned_list.dart';
 import 'package:wakelock/wakelock.dart';
 
 import '../../models/recipe.dart';
 import '../../models/recipe_ingredient.dart';
 import '../../models/recipe_step.dart';
-import '../../widgets/screen_body.dart';
 
 class RecipeInstructionScreen extends StatefulWidget {
   const RecipeInstructionScreen({Key key, @required this.recipe})
@@ -32,51 +32,68 @@ class _RecipeInstructionScreenState extends State<RecipeInstructionScreen> {
 
   @override
   Widget build(BuildContext context) {
+    final itemScrollController = ItemScrollController();
+    final itemPositionsListener = ItemPositionsListener.create();
+
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.recipe.name),
       ),
-      body: ScreenBody(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            _buildSteps(context),
-            RaisedButton.icon(
-              onPressed: () => Navigator.pop(context),
-              icon: const Icon(Icons.check_circle_outline),
-              label: const Text('Done!'),
-            ),
-          ],
+      body: Scrollbar(
+        child: Container(
+          padding: const EdgeInsets.symmetric(vertical: 8),
+          decoration: BoxDecoration(color: Colors.grey.shade100),
+          child: ScrollablePositionedList.builder(
+            itemScrollController: itemScrollController,
+            itemPositionsListener: itemPositionsListener,
+            itemCount: widget.recipe.steps.length + 1,
+            itemBuilder: (context, index) => index == widget.recipe.steps.length
+                ? Container(
+                    padding: const EdgeInsets.symmetric(horizontal: 16),
+                    child: RaisedButton.icon(
+                      onPressed: () => Navigator.pop(context),
+                      icon: const Icon(Icons.check_circle_outline),
+                      label: const Text('Done!'),
+                    ),
+                  )
+                : _buildStep(
+                    context,
+                    widget.recipe.steps[index],
+                    onTap: () => itemScrollController.scrollTo(
+                      index: index + 1,
+                      duration: const Duration(milliseconds: 100),
+                    ),
+                  ),
+          ),
         ),
       ),
     );
   }
 
-  Widget _buildSteps(BuildContext context) {
-    return Column(children: [
-      ...widget.recipe.steps.map(
-        (step) => _buildStep(context, step),
-      ),
-    ]);
-  }
-
-  Widget _buildStep(BuildContext context, RecipeStep step) {
-    return Card(
-      margin: const EdgeInsets.symmetric(vertical: 16),
-      child: Container(
-        padding: const EdgeInsets.all(16),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            Text(
-              step.description,
-              style: Theme.of(context).textTheme.subtitle1,
-            ),
-            Container(
-              margin: const EdgeInsets.only(top: 16),
-              child: _buildIngredients(context, step.ingredients),
-            ),
-          ],
+  Widget _buildStep(
+    BuildContext context,
+    RecipeStep step, {
+    Future<void> Function() onTap,
+  }) {
+    return InkWell(
+      onTap: () => onTap(),
+      child: Card(
+        margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+        child: Container(
+          padding: const EdgeInsets.all(16),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Text(
+                step.description,
+                style: Theme.of(context).textTheme.subtitle1,
+              ),
+              Container(
+                margin: const EdgeInsets.only(top: 16),
+                child: _buildIngredients(context, step.ingredients),
+              ),
+            ],
+          ),
         ),
       ),
     );
